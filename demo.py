77816101f097a5b90a726cb24c242793b6c5cf07
Ramya <ramyaphone78@gmail.com>
2018-10-29 20:18:22 +0530

    templates and layout

--- demo.py
+++ demo.py
@@ -1,13 +1,24 @@
-from flask import Flask
+from flask import Flask,render_template
 app = Flask(__name__)
+
+posts = [
+    {
+        'name' : "Ramya",
+        'title' : "Cryptography in the era of quantum computing"
+    },
+    {
+        'name' : "Grace",
+        'title' : "Something"
+    }
+]
 
 @app.route("/")
 def hello():
-    return "<h1>Hello World!</h1>"
+    return render_template('home.html',posts=posts)
 
 @app.route("/about")
 def about():
-    return "<h1>about</h1>"
+    return render_template('about.html',posts=posts)
 
 if __name__ == '__main__':
     app.run(debug=True)
